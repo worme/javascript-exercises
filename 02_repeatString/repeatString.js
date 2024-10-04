93fde376a0e795b1b780191d1b39d9b1eda8a245
Jason S. <52556746+worme@users.noreply.github.com>
2024-10-04 18:38:34 -0500

    Update function with my solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    if (num < 0) {
+        newString = "ERROR";
+    } else {
+    
+        for (let index = 0; index < num; index++) {
+            newString = newString + string; 
+        }
+    }
+    return newString;   
 };
 
 // Do not edit below this line
