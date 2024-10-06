2ec23cd330aa72e0add8f9bf0436b4091eb91b81
Jason S. <52556746+worme@users.noreply.github.com>
2024-10-05 23:33:29 -0500

    Update function with my working implementation

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,13 +1,19 @@
 const reverseString = function(inString) {
     let newString = "";
+    let lastElement = -1;
     for (let index = 0; index < inString.length; index++) {
         const element = inString[index];
-        
-        console.log(element)
-        
+        // console.log(element)
+        if (newString === "") { 
+            newString = newString + inString.slice(lastElement);
+        } else {
+            newString = newString + inString.slice(lastElement, lastElement+1);
+        }
+        lastElement--;
     }
+    return newString;
 };
 
 // Do not edit below this line
-// module.exports = reverseString;
-reverseString("Hello!")
+module.exports = reverseString;
+// reverseString("Hello!")
--- 03_reverseString/reverseString.spec.js
+++ 03_reverseString/reverseString.spec.js
@@ -1,20 +1,20 @@
 const reverseString = require('./reverseString');
 
 describe('reverseString', () => {
-  test.skip('reverses single word', () => {
+  test('reverses single word', () => {
     expect(reverseString('hello')).toEqual('olleh');
   });
 
-  test.skip('reverses multiple words', () => {
+  test('reverses multiple words', () => {
     expect(reverseString('hello there')).toEqual('ereht olleh');
   });
 
-  test.skip('works with numbers and punctuation', () => {
+  test('works with numbers and punctuation', () => {
     expect(reverseString('123! abc! Hello, Odinite.')).toEqual(
       '.etinidO ,olleH !cba !321'
     );
   });
-  test.skip('works with blank strings', () => {
+  test('works with blank strings', () => {
     expect(reverseString('')).toEqual('');
   });
 });
